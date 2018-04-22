d9bb0bb2bbd833f3b5392f66c5accbe346210847
zhanghonghua <457285383@qq.com>
2018-04-22 15:24:11 +0800

    完成第六章

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -15,25 +15,34 @@ from rango.models import Category,Page
 def populate():
     python_pages = [
             {"title":"Official Python Tutorial",
-             "url":"http://docs.python.org/3/tutorial"},
+             "url":"http://docs.python.org/3/tutorial",
+             'views':100},
+
              {"title":"How To Think Like a Computer Scientist",
-              "url":"http://www.greenteapress.com/thinkpython/"},
+              "url":"http://www.greenteapress.com/thinkpython/",
+              'views':100},
               {"title":"Learn Python in 10 Minutes",
-               "url":"http://www.korokithakis.net/tutorials/python/"}
+               "url":"http://www.korokithakis.net/tutorials/python/",
+               'views':100}
               ]
     django_pages = [
             {"title":"Official Django Tutorial",
-             "url":"https://docs.djangoproject.com/zh-hans/2.0/intro/"},
+             "url":"https://docs.djangoproject.com/zh-hans/2.0/intro/",
+             'views':80},
             {"title":"Django Rocks",
-             "url":"http://www.djangorocks.com/"},
+             "url":"http://www.djangorocks.com/",
+             'views':80},
              {"title":"How to Tango With Django",
-              "url":"http://www.tangowithdjango.com/"}
+              "url":"http://www.tangowithdjango.com/",
+              'views':80}
             ]
     other_pages = [
             {"title":"Bottle",
-             "url":"http://bottlepy.org/docs/dev"},
+             "url":"http://bottlepy.org/docs/dev",
+             'views':60},
              {"title":"Flask",
-             "url":"http://flask.pocoo.org"}
+             "url":"http://flask.pocoo.org",
+             'views':60}
              ]
     cats = {"Python":{"pages":python_pages,"views":128,"likes":64},
             "Django":{"pages":django_pages,"views":64,"likes":32},
@@ -43,7 +52,7 @@ def populate():
     for cat ,cat_data in cats.items():
         c = add_cat(cat,cat_data["views"],cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c,p["title"],p["url"])
+            add_page(c,p["title"],p["url"],p["views"])
             
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
         path('',views.index,name='index'),
         path('about/',views.about,name='about'),
+        re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),#?P<name>pattern ,named regular expression groups
         ] 
