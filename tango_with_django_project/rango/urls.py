4788aacf27476db81dfb74a772b7d890329f7f43
zhanghonghua <457285383@qq.com>
2018-05-03 14:28:50 +0800

    完成第七章

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
         path('',views.index,name='index'),
         path('about/',views.about,name='about'),
+        re_path(r'^add_category/$',views.add_category,name='add_category'),
+        re_path(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page,name='add_page'),
         re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category,name='show_category'),#?P<name>pattern ,named regular expression groups
         ] 
